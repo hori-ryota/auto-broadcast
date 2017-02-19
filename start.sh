2775d74c89340c75822cca7e56be5d4c6e2a1bc5
hori-ryota <hori_ryota@cyberagent.co.jp>
2017-02-19 18:23:43 +0900

    fix mp4 SAR

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 echo "target = '${TARGET_RTMP_URL}'"
 
-ffmpeg -re -stream_loop -1 -i 'src.mp4' -vf "drawtext=fontfile=./NotoSans-Bold.ttf: text='%{localtime\:%F%n%n%T}': fontcolor=black@0.8:fontsize=64: x=32: y=64" -vcodec libx264 -x264-params keyint=30 -acodec aac -f flv "${TARGET_RTMP_URL}"
+ffmpeg -re -stream_loop -1 -i 'src.mp4' -vf "drawtext=fontfile=./NotoSans-Bold.ttf: text='%{localtime\:%FT%T}': fontcolor=black@0.8:fontsize=64: x=32: y=32" -vcodec libx264 -x264-params keyint=30 -acodec aac -f flv "${TARGET_RTMP_URL}"
